a4865bff930cf4299e10185a314f05e42bdae4fc
krishsah01 <157822205+krishsah01@users.noreply.github.com>
2025-04-29 11:06:32 -0400

    put node module in gitignore

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RPS from "./pages/RockPaperScissors/RPS";
+import TicTacToe from "./pages/TicTacToe/TicTacToe";
+import HomePage from "./HomePage";
+
+import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
+import LoginPage from "./pages/Login/LoginPage";
+import Snake from "./pages/Snake/Snake";
+import WordleGame from "./pages/Wordle/Wordle";
+import Hangman from "./pages/Hangman/App"
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Navigation />
+      <div className="app-container">
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route index element={<HomePage />} />
+          <Route path="/Rock-Paper-Scissors" element={<RPS/>} />
+          <Route path="/tic-tac-toe" element={<TicTacToe />} />
+          <Route path="/wordle" element={<WordleGame />} />
+          <Route path="/snake" element={<Snake />} />
+          <Route path="/hangman" element={<Hangman />} />
+        </Routes>
+      </div>
+      <div className="footer">
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useEffect, useState} from "react";
+import { useNavigate } from "react-router-dom";
+
+
+const Navigation = () => {
+  const [username, setUsername] = useState("");
+  const navigate = useNavigate();
+
+  const storedUserName = localStorage.getItem("username");
+  useEffect(() => {
+    if (storedUserName) {
+      setUsername(storedUserName);
+    }else {
+      setUsername("");
+    }
+  }, [storedUserName]);
+
+
+  const handleLogout = () => {
+    localStorage.removeItem("username");
+    setUsername("");
+    navigate("/login");
+  };
+  return (
+    <nav>
+      <ul className="navigation-list">
+
+        {!username ? (
+          <li><Link to="/login">Login</Link></li>
+        ) : (
+          <>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/Rock-Paper-Scissors">Rock Paper Scissors</Link></li>
+            <li><Link to="/tic-tac-toe">Tic Tac Toe</Link></li>
+            <li><Link to="/wordle">Wordle</Link></li>
+            <li><Link to="/snake">Snake</Link></li>
+            <li><Link to="/hangman">Hangman</Link></li>
+              <button onClick={handleLogout} className="logout-button">Logout</button>
+          </>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
